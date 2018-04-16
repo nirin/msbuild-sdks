9cd286ee0316e373a9ddb2f1f110558ae8e07cf9
Nirmal Guru <Nirmal4G@gmail.com>
2018-04-16 08:13:01 +0530

    Added Templates/TestProjects
    for MSBuild.NET.Extras.Sdk

---
+++ TestProjects/Windows.Service.Application/MainService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSBuild.TestProjects.WinServiceApp
+{
+	public partial class MainService : ServiceBase
+	{
+		public MainService()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnStart(string[] args)
+		{
+
+		}
+
+		protected override void OnStop()
+		{
+
+		}
+	}
+}
